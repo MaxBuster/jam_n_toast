8d70d79ae8fc407646ad044151fb35df04bf790c
Max Buster <mkbuster@wustl.edu>
2015-09-20 02:15:54 -0500

    Added a distance field

--- api_call.js
+++ api_call.js
@@ -87,6 +87,9 @@ function getCityInfo(cityID) {
 	 //  	});
 
 	 //  	locationArray["activities"] = activityArray;
+
+		document.getElementById("distance").innerHTML = Math.round(distance(-90.19, 38.62, data.latitude, data.longitude, "K")) + " km";
+
 	  	return locationArray;
 	  },
 	  failure: function(data) {
@@ -168,4 +171,20 @@ function getContinent(priceRange) {
 	var randomNum = Math.round(priceRange*Math.random());
 	var continents = [19, 291958, 13, 8, 6, 21, 2, 4];
 	return continents[randomNum];
+}
+
+function distance(lat1, lon1, lat2, lon2, unit) {
+	var radlat1 = Math.PI * lat1/180
+	var radlat2 = Math.PI * lat2/180
+	var radlon1 = Math.PI * lon1/180
+	var radlon2 = Math.PI * lon2/180
+	var theta = lon1-lon2
+	var radtheta = Math.PI * theta/180
+	var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+	dist = Math.acos(dist)
+	dist = dist * 180/Math.PI
+	dist = dist * 60 * 1.1515
+	if (unit=="K") { dist = dist * 1.609344 }
+	if (unit=="N") { dist = dist * 0.8684 }
+	return dist
 }
